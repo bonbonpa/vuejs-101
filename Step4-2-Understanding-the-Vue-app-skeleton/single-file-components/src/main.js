fe03a48b740c7e5390bfdde93c167362f495e486
bonbonpa <darksphin@gmail.com>
2017-05-14 17:10:04 +0700

    update main.js add components

--- Step4-2-Understanding-the-Vue-app-skeleton/single-file-components/src/main.js
+++ Step4-2-Understanding-the-Vue-app-skeleton/single-file-components/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App),
+    components: {
+        app: App
+    }
 })
